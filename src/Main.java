b91833756ce8a1529afad75cb7463eba6e5c38a9
SAROJKARK0321 <sarojkarki0321@gmail.com>
2025-02-15 17:45:57 -0600

    new changes in branch

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome! paji");
 
-        for (int i = 1; i <= 21) {
+        for (int i = 5; i <= 500) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println(i+"hello ji kaise hoq");
-            i=i+3;
+            i=i+50;
         }
     }
 }
